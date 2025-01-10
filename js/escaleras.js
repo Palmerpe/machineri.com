cb40d33e545ba9cdc5c4b06293b2bf242e84eef3
palmerpe <mathiasp.liam@gmail.com>
2025-01-09 22:49:29 -0500

    terminando escaleras.html, techos y nontaje-desmontaje y algunos cambios extras

---
+++ js/escaleras.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const texts = ['Industriales', 'De Emergencia', 'Flotantes', 'Modulares', 'Carcol y mas...'];
+    let count = 0;
+    let index = 0;
+    let currentText = '';
+    let letter = '';
+    
+    (function type() {
+      if (count === texts.length) {
+        count = 0;
+      }
+      currentText = texts[count];
+      letter = currentText.slice(0, ++index);
+  
+      document.getElementById('dynamic-text').textContent = letter;
+      if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+        setTimeout(type, 2000);
+      } else {
+        setTimeout(type, 150);
+      }
+    })();
+  });
