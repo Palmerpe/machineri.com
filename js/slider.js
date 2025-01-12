529af58b0e539f3419d1bd13ab241946213bd913
palmerpe <mathiasp.liam@gmail.com>
2025-01-12 12:59:01 -0500

    Ajustar estilos del slider y mejorar la funcionalidad del JavaScript

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Selecciona el contenedor del slider y las imágenes
   const track = document.querySelector('.slider-track');
   const slides = document.querySelectorAll('.slider-track img');
 
-  // Índice actual del slider
   let index = 0;
-
-  // Ancho de cada imagen (asumiendo que todas tienen el mismo ancho)
   let slideWidth = slides[0].clientWidth;
 
-  // Función para actualizar la posición del slider
   const updateSlider = () => {
       index++;
       track.style.transition = 'transform 0.7s ease-in-out';
@@ -20,22 +15,22 @@ document.addEventListener('DOMContentLoaded', function () {
               track.style.transition = 'none';
               track.style.transform = 'translateX(0)';
               index = 0;
-          }, 700); // Duración de la transición en milisegundos
+          }, 700); 
       }
   };
 
-  // Cambia de imagen cada 4 segundos
   setInterval(updateSlider, 4000);
 
-  // Ajustar el ancho de las imágenes al cambiar el tamaño de la ventana
   window.addEventListener('resize', () => {
       slideWidth = slides[0].clientWidth;
       track.style.transition = 'none';
       track.style.transform = `translateX(-${index * slideWidth}px)`;
       setTimeout(() => {
           track.style.transition = 'transform 0.7s ease-in-out';
-      }, 50); // Ajuste de tiempo para asegurar la transición
+      }, 50);
   });
 });
+
+
 
 
