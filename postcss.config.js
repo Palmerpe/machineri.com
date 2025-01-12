cb941fc3fd533d4916c0c0805c2909ad031ee57e
palmerpe <mathiasp.liam@gmail.com>
2025-01-12 11:46:12 -0500

    Compilar CSS, ajustar configuración y actualizar dependencias

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 
 module.exports = {
     plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
         require('cssnano')({
             preset: 'default',
         }),
     ],
 };
+
+
