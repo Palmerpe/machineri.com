78c319177b4fb154f95aae405ed80d4670ac487a
palmerpe <mathiasp.liam@gmail.com>
2025-01-14 17:13:48 -0500

    Actualizar footer y enlaces en todas las plantillas

--- js/slider.js
+++ js/slider.js
@@ -2,34 +2,40 @@ document.addEventListener('DOMContentLoaded', function () {
   const track = document.querySelector('.slider-track');
   const slides = document.querySelectorAll('.slider-track img');
 
+  if (slides.length === 0) {
+    console.error('No se encontraron imágenes para el slider.');
+    return;
+  }
+
   let index = 0;
   let slideWidth = slides[0].clientWidth;
 
   const updateSlider = () => {
-      index++;
-      track.style.transition = 'transform 0.7s ease-in-out';
-      track.style.transform = `translateX(-${index * slideWidth}px)`;
+    index++;
+    track.style.transition = 'transform 0.7s ease-in-out';
+    track.style.transform = `translateX(-${index * slideWidth}px)`;
 
-      if (index >= slides.length / 2) {
-          setTimeout(() => {
-              track.style.transition = 'none';
-              track.style.transform = 'translateX(0)';
-              index = 0;
-          }, 700); 
-      }
+    if (index >= slides.length / 2) {
+      setTimeout(() => {
+        track.style.transition = 'none';
+        track.style.transform = 'translateX(0)';
+        index = 0;
+      }, 700);
+    }
   };
 
   setInterval(updateSlider, 4000);
 
   window.addEventListener('resize', () => {
-      slideWidth = slides[0].clientWidth;
-      track.style.transition = 'none';
-      track.style.transform = `translateX(-${index * slideWidth}px)`;
-      setTimeout(() => {
-          track.style.transition = 'transform 0.7s ease-in-out';
-      }, 50);
+    slideWidth = slides[0].clientWidth;
+    track.style.transition = 'none';
+    track.style.transform = `translateX(-${index * slideWidth}px)`;
+    setTimeout(() => {
+      track.style.transition = 'transform 0.7s ease-in-out';
+    }, 50);
   });
 });
+
 
 
 
